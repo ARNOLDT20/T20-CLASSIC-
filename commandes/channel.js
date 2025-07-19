f9b782024f2affc979781070f2d17bca565ad5ea
JEEPERS CREEPER-XMD <Alonetech111@gmail.com>
2025-07-19 10:58:10 +0300

    Create channel.js

---
+++ commandes/channel.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+
+zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Salut je m\'appelle *t10 classic ai* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let d = ' developpé par *🥇Charles*';
+    let varmess = z + d;
+    var lien = 'https://whatsapp.com/channel/0029Vb6W30X1noyy6cijtB1q';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    await zk.sendMessage(dest, { text: varmess + "\n" + lien });
+});
+
+console.log("mon test");
+
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *classic ai* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *t20*'
+      let varmess=z+d
+      var img='https://i.imgur.com/jE8eQsP.jpeg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
