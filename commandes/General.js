485456fa7fd210fd9a3478c24f1f9efd9f65d328
JEEPERS CREEPER-XMD <Alonetech111@gmail.com>
2025-07-19 10:21:27 +0300

    Create General.js

---
+++ commandes/General.js
@@ -0,0 +1,101 @@
+const { zokou } = require("../framework/zokou");
+const {getAllSudoNumbers,isSudoTableNotEmpty} = require("../bdd/sudo")
+const conf = require("../set");
+
+zokou({ nomCom: "owner", categorie: "General", reaction: "🤝" }, async (dest, zk, commandeOptions) => {
+    const { ms , mybotpic } = commandeOptions;
+    
+  const thsudo = await isSudoTableNotEmpty()
+
+  if (thsudo) {
+     let msg = `*My Super-User*\n
+     *Owner Number*\n :
+- 🌟 @${conf.NUMERO_OWNER}
+
+------ *other sudos* -----\n`
+     
+ let sudos = await getAllSudoNumbers()
+
+   for ( const sudo of sudos) {
+    if (sudo) { // Vérification plus stricte pour éliminer les valeurs vides ou indéfinies
+      sudonumero = sudo.replace(/[^0-9]/g, '');
+      msg += `- 💼 @${sudonumero}\n`;
+    } else {return}
+
+   }   const ownerjid = conf.NUMERO_OWNER.replace(/[^0-9]/g) + "@s.whatsapp.net";
+   const mentionedJid = sudos.concat([ownerjid])
+   console.log(sudos);
+   console.log(mentionedJid)
+      zk.sendMessage(
+        dest,
+        {
+          image : { url : mybotpic() },
+          caption : msg,
+          mentions : mentionedJid
+        }
+      )
+  } else {
+    const vcard =
+        'BEGIN:VCARD\n' + // metadata of the contact card
+        'VERSION:3.0\n' +
+        'FN:' + conf.OWNER_NAME + 'T20 CLASSIC AI verified ✅\n' + // full name
+        'ORG:undefined;\n' + // the organization of the contact
+        'TEL;type=CELL;type=VOICE;waid=' + conf.NUMERO_OWNER + ':+254759626063' + conf.NUMERO_OWNER + '254759626063\n' + // WhatsApp ID + phone number
+        'END:VCARD';
+    zk.sendMessage(dest, {
+        contacts: {
+            displayName: conf.OWNER_NAME,
+            contacts: [{ vcard }],
+        },
+    },{quoted:ms});
+  }
+});
+
+zokou({ nomCom: "dev", categorie: "General", reaction: "🚘" }, async (dest, zk, commandeOptions) => {
+    const { ms, mybotpic } = commandeOptions;
+
+    const devs = [
+      { nom: "T20", numero: "255 768 418 867" },
+      { nom: "᚛sir bravin᚜", numero: "254717263689" },
+      { nom: "T20", numero: "255 627 417 402" },
+      // Ajoute d'autres développeurs ici avec leur nom et numéro
+    ];
+
+    let message = "WELCOME T20 CLASSIC AI HELP CENTER! ASK FOR HELP FROM ANY OF THE DEVELOPERS BELOW:\n\n";
+    for (const dev of devs) {
+      message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
+    }
+  var lien = mybotpic()
+    if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:message }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:message }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    repondre(lien)
+    repondre("link error");
+    
+}
+});
+
+zokou({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, auteurMessage, } = commandeOptions; 
+ 
+  repondre("THANK YOU FOR CHOOSING T20 CLASSIC AI, HERE ARE OUR SUPPORTIVE LINKS\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[ https://whatsapp.com/channel/0029Vb6W30X1noyy6cijtB1q ] \n\n ☉ GROUP LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[ https://https://chat.whatsapp.com/EFh0BWKa0Uf92A7RgBwpuu ] \n\n ☉YOUTUBE LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://youtube.com/@charles_odhiambo_official?si=n6lKIy2ViQauVS_n] \n\n\n𝑪𝒓𝒆ated by ®Charles ke") 
+  await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING T20 CLASSIC AI,MAKE SURE YOU FOLLOW THESE LINKS. `},{quoted :ms})
+
+})
