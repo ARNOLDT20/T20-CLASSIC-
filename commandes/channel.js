2e9055f7b33ed6d4de2186dd4db2f6c21012bc5b
JEEPERS CREEPER-XMD <Alonetech111@gmail.com>
2025-07-20 18:02:52 +0300

    Update channel.js

--- commandes/channel.js
+++ commandes/channel.js
@@ -5,7 +5,7 @@ const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'Salut je m\'appelle *t10 classic ai* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
-    let d = ' developpé par *🥇Charles*';
+    let d = ' developpé par *🥇t20*';
     let varmess = z + d;
     var lien = 'https://whatsapp.com/channel/0029Vb6W30X1noyy6cijtB1q';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
