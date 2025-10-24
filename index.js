e6012f7541ce90597614ce0117fa715888fe93ef
Sila-Md <hackersila2@gmail.com>
2025-10-24 11:16:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -93,8 +93,8 @@ async function loadSession() {
         console.log('[🔰] Downloading MEGA.nz session...');
         
         // Remove "IK~" prefix if present, otherwise use full SESSION_ID
-        const megaFileId = config.SESSION_ID.startsWith('IMMU~') 
-            ? config.SESSION_ID.replace("IMMU~", "") 
+        const megaFileId = config.SESSION_ID.startsWith('Silva~') 
+            ? config.SESSION_ID.replace("Silva~", "") 
             : config.SESSION_ID;
 
         const filer = File.fromURL(`https://mega.nz/file/${megaFileId}`);
