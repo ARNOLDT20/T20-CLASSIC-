f5b8d5f27c004b1dcb2d65b5787cb8038bbd9d96
ARNOLD <atarimo117@gmail.com>
2025-10-27 13:46:20 +0300

    Update response text in main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *IMMU MD SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *T20-CLASSIC-AI SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
